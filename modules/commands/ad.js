7c44ecf9e77a48f90622306666073110dc68797e
trandung29 <84967087+trandung29@users.noreply.github.com>
2021-11-09 18:54:47 +0900

    first commit

---
+++ modules/commands/ad.js
@@ -0,0 +1,11 @@
+﻿module.exports.config = {
+	name: "ad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Hà Mạc Trường Giang",
+	description: "Thông tin về admin",
+	commandCategory: "Thông tin về admin",
+	cooldowns: 0
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`\n★Thông Tin Admin Bot Này★\nADMIN NAME : SángBiz \nBiệt Danh: SBIZ ~~\nGiới Thiệu: Nhà anh không có gì ngoài nợ ngân hàng 10 tỉ\nI Have Something To You\nI Love You\nTính Cách : Cục súc với thiên hạ nhưng lại chỉ dịu dàng với mỗi Ly\nChiều cao : 1m74\nSinh ngày : 12/03/2005\nLiên hệ:+84********* \nInstagram: #####\nTikTok: yeulyynhut\nSở Thích: Thích chơi game, anime\nCân nặng: 50kg\nUID FACEBOOK : 100045223667485\nName ID : Sáng \nLink Facebook : https://www.facebook.com/yeuly0409\nVài lời tới người dùng BOT: Vui lòng không spam khi sử dụng để tránh die bot. Cảm ơn mọi người đã sử dụng đên con bot của mình.\nLưu ý : Đừng có dại dột mà add 2 bot kẻo bị phát hiện là bạn toang đó <3\nCảnh báo : Vui lòng không dùng bot với mục đích xấu hay cố ý report acc facebook\nChúc bạn sử dụng vui vẻ <3\n=== SángBiz ( Cre P ) ===`, event.threadID, event.messageID);
